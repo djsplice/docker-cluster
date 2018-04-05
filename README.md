6c99160ebf11a7b1f04b850ee7c6fd50ed64064f
Jeff Barrows <barrows.jeff@gmail.com>
2018-04-05 05:53:07 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# docker-swarm
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: docker-swarm
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
